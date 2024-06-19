ed6213bebaf3872deecafa37bc0ac4e240a488b8
AbhiAuti <131663587+AbhiAuti@users.noreply.github.com>
2024-06-19 19:55:56 +0530

    Update AuthForm.jsx

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -17,7 +17,7 @@ const AuthForm = () => {
         </VStack>
       </Box>
       
-      <Box border={"1px solid gray"} borderRadius={4} padding={5} marginTop={4}>
+      <Box border={"2px solid black"} borderRadius={4} padding={5} marginTop={4}>
         <Flex alignItems={"center"} justifyContent={"center"}>
           <Box mx={2} fontSize={18} color={"black"}>
             {isLogin ? "Don't have an account?" : "Already have an account?"}
